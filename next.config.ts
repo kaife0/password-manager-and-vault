c88a174388cc2c03d33afeaad7e3c168457b3c51
kaife0 <mdkaif1590@gmail.com>
2025-10-05 14:14:39 +0530

    fix

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   poweredByHeader: false,
+  outputFileTracingRoot: __dirname,
   async headers() {
     return [
       {
